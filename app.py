f7456bdddbcaa46f1da020fbf82c2fd4f7aeb671
ykurniandha@gmail.com <ykurniandha@gmail.com>
2022-10-13 18:10:08 +0700

    add map, sklearn, and calculator

--- app.py
+++ app.py
@@ -5,24 +5,149 @@ import plotly.graph_objects as go
 from streamlit_plotly_events import plotly_events
 
 import util
+import mapping
 
 # Konfigurasi Halaman
 st.set_page_config(page_title="Peta Indonesia", layout="wide")
+st.title('Capaian Indikator Utama Pembangunan')
 
-st.header('Peta Indeks Pembangunan Manusia per Provinsi')
-df_metric = pd.read_csv('data.csv')
-df, indomap = util.read_map(df_metric)
+tab1, tab2 = st.tabs(["Pemrosesan Data", "Hasil"])
+# Pemrosesan Data
+#df = util.read_and_preprocess_data('IPM')
+# =================================================================
+with tab1:
+    inputcol1, inputcol2 = st.columns(2)
 
-peta = util.plot_map(df, indomap)
-selected_points = plotly_events(peta)
+    with inputcol1:
+        tahun = st.selectbox('Tahun', ([str(x) for x in range(2010, 2022)]))
 
-if(len(selected_points) > 0):
-    idx = int(selected_points[0]['pointIndex'])
+    with inputcol2:
+        target = st.selectbox('Target', ('Indeks Pembangunan Manusia', 'Tingkat Kemiskinan', 'Gini Rasio', 'Laju Pertumbuhan Ekonomi', 'Tingkat Pengangguran Terbuka'))
 
-    st.success(df.iloc[idx]['provinsi'])
+    filetarget = 'Indeks Pembangunan Manusia.xlsx'
+    column = ['IPM']
 
-    col1, col2, col3, col4 = st.columns(4)
-    col1.metric("IPM", str(df.loc[df['provinsi'] == df.iloc[idx]['provinsi']]['ipm'].iloc[0]))
-    col2.metric("a", str(df.loc[df['provinsi'] == df.iloc[idx]['provinsi']]['a'].iloc[0]))
-    col3.metric("b", str(df.loc[df['provinsi'] == df.iloc[idx]['provinsi']]['b'].iloc[0]))
-    col4.metric("c", str(df.loc[df['provinsi'] == df.iloc[idx]['provinsi']]['c'].iloc[0]))
+    if (target == 'Tingkat Kemiskinan'):
+        filetarget = 'persentasemiskin.xlsx'
+        column = ['Kemiskinan']
+    elif (target == 'Gini Rasio'):
+        filetarget = 'giniratio.xlsx'
+        column = ['Gini']
+    elif (target == 'Laju Pertumbuhan Ekonomi'):
+        filetarget = 'Laju PDRB.xlsx'
+        column = ['LPE']
+    elif (target == 'Tingkat Pengangguran Terbuka'):
+        filetarget = 'pengangguran.xlsx'
+        column = ['TPT']
+
+    exec('{} = pd.read_excel("{}")'.format(column[0], filetarget))
+    provinsi=['ACEH','SUMATERA_UTARA','SUMATERA_BARAT','RIAU', 'JAMBI',	'SUMATERA_SELATAN',	'BENGKULU',	'LAMPUNG',	'BANGKA_BELITUNG',	'KEPRI',	'DKI_JAKARTA',	'JAWA_BARAT',	'JAWA_TENGAH',	'DI_YOGYAKARTA',	'JAWA_TIMUR',	'BANTEN',	'BALI',	'NTB',	'NTT',	'KALIMANTAN_BARAT',	'KALIMANTAN_TENGAH',	'KALIMANTAN_SELATAN',	'KALIMANTAN_TIMUR',	'KALIMANTAN_UTARA',	'SULAWESI_UTARA',	'SULAWESI_TENGAH',	'SULAWESI_SELATAN',	'SULAWESI_TENGGARA',	'GORONTALO',	'SULAWESI_BARAT',	'MALUKU',	'MALUKU_UTARA',	'PAPUA_BARAT',	'PAPUA']
+
+    for x in provinsi:  
+        exec('{} = pd.DataFrame(columns=column)'.format(x))
+
+    countX=0
+    countY=0
+
+    #isi data target
+    for i in provinsi:
+        for j in column:
+            if ((column[0] == 'IPM') or (column[0] == 'LPE')):
+                exec('{}["{}"]={}.loc[{}:{},2010:2021].T'.format(i,j,j,countX,countY))
+            else:
+                exec('{}["{}"]={}.loc[{}:{},2021:2010].T'.format(i,j,j,countX,countY))
+        #countX+=1
+    countY+=1
+
+    APBN = pd.read_csv('Peta APBN Data.csv', header=None)
+
+    APBN[0][0] = 'Tahun'
+    APBN.columns = APBN.iloc[0]
+    APBN = APBN.iloc[1:]
+    APBN = APBN.astype({"Tahun": int})
+    APBN = APBN.astype({"Tahun": str})
+    APBN = APBN.set_index('Tahun', drop=True)
+
+    mappingprovinsiAPBN = mapping.mappingprovinsiAPBN
+
+    # Pisah2 --> 11 fitur APBN
+    for x in mappingprovinsiAPBN.keys():
+        firstcol = (x-1)*11 # ini parameter yang diganti2 kalo ganti jumlah kolom
+        lastcol = x*11 # ini parameter yang diganti2 kalo ganti jumlah kolom
+        exec('{} = APBN.iloc[:, {}:{}]'.format(mappingprovinsiAPBN[x], firstcol, lastcol))
+        exec('for col in {}.columns: {}[col] = {}[col].astype(int)'.format(mappingprovinsiAPBN[x],mappingprovinsiAPBN[x],mappingprovinsiAPBN[x]))
+        #exec('for col in {}.columns: {}.loc[:, col] *= 1000000'.format(mappingprovinsiAPBN[x],mappingprovinsiAPBN[x],mappingprovinsiAPBN[x]))
+
+    # Join2 --> kolom target dengan 11 fitur APBN
+    for x in provinsi:
+        exec('{}.index = {}.index.map(str)'.format(x, x))
+        exec('{} = pd.concat([{}, {}APBN], axis=1, join="inner")'.format(x, x, x))
+    # =================================================================
+
+    # Pemrosesan Peta
+    st.success('Data ' + target + ' per Provinsi')
+    exec('df_mapping = {}'.format(column[0]))
+    df, indomap = util.read_map(df_mapping, provinsi)
+
+    peta = util.plot_map(df, indomap, tahun)
+    selected_points = plotly_events(peta)
+
+    # Visualisasi Peta
+    if(len(selected_points) > 0):
+        idx = int(selected_points[0]['pointIndex'])
+
+        name_provinsi = df.iloc[idx]['provinsi']
+        selected_provinsi = df['variabel'].iloc[idx]
+
+        #exec('st.write({})'.format(selected_provinsi))
+        exec('results = util.prediction({})'.format(selected_provinsi))
+
+with tab2:
+    if 'name_provinsi' in locals():
+        st.subheader('Capaian ' + column[0] + ' pada provinsi ' + name_provinsi)
+    if 'results' in locals():
+        st.success('Hasil Prediksi ' + column[0])
+        col1, col2, col3, col4 = st.columns(4)
+        with col1:
+            st.write('Hasil Prediksi')
+            st.write(results['y_test'])
+        with col2:
+            st.write('Data Real')
+            st.write(results['y_pred'])
+        col3.metric("Mean Square Error (MSE)", str('{:.2f}'.format(results['MSE'])))
+
+        st.success('Fitur Importance')
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write('Feature Importance')
+            st.write(results['importance_df'].style.applymap(util.is_feature_importance, subset=['importance']))
+            #df.style.applymap(is_temp_valid, subset=['celsius_temperature'])
+        with col2:
+            st.write('Best Features')
+            st.write(results['dfprov'])
+
+        st.success('Hasil Prediksi dengan Best Features')
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.write('Hasil Prediksi')
+            st.write(results['y_test Tahap 2'])
+        with col2:
+            st.write('Data Real')
+            st.write(results['y_pred Tahap 2'])
+        col3.metric("Mean Square Error (MSE)", str('{:.2f}'.format(results['mse Tahap 2'])))
+
+        st.success('Simulasi Kalkulator')
+        with st.form("my_form"):
+            col1, col2, col3 = st.columns(3)
+            with col1:
+                f1 = st.number_input('Fitur ' + results['dfprov'].columns[1])
+            with col2:
+                f2 = st.number_input('Fitur ' + results['dfprov'].columns[2])
+            with col3:
+                f3 = st.number_input('Fitur ' + results['dfprov'].columns[3])
+                
+            submitted = st.form_submit_button("Submit")
+            if submitted:
+                predictionresult = (f1*results['regressor.coef_'][0][0] + f2*results['regressor.coef_'][0][1] + f3*results['regressor.coef_'][0][2])
+                st.metric('Hasil Prediksi:', str(predictionresult))
+                
--- util.py
+++ util.py
@@ -1,35 +1,52 @@
 import json
+import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
+from execbox import execbox
+from sklearn import datasets, ensemble
+from sklearn.inspection import permutation_importance
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
+import mapping
 
-def read_map(df_metric):
+def read_map(df_mapping, provinsi):
     f = open('indonesia.geojson')  
     indomap = json.load(f)
     f.close()
+
+    provtemp = provinsi
+    provtemp.append('INDONESIA')
+
+    df_mapping['variabel'] = provtemp
 
     df = pd.DataFrame(
         {"Provinsi": pd.json_normalize(indomap["features"])["properties.state"]}
     ).assign(Columnnext=lambda d: d["Provinsi"].str.len())
     
+    mappingprovinsi = pd.read_csv('mappingprovinsi.csv')
+
     dfnew = pd.DataFrame()
 
-    for i in range (len(df)):
-        df_metric_temp = df_metric.loc[df_metric['provinsi'] == df['Provinsi'].iloc[i]]
-        dfnew = dfnew.append(df_metric_temp)
-    
-    print(dfnew)
+    for i in mappingprovinsi['mappingtarget']:
+        df_mapping_temp = df_mapping.iloc[i]
+        dfnew = dfnew.append(df_mapping_temp)
+
+    dfnew = dfnew.reset_index(drop=True)
+    dfnew = pd.concat([dfnew, mappingprovinsi['provinsi']], axis=1, join="inner")
 
     return dfnew, indomap
 
-def plot_map(df, indomap):
+def plot_map(df, indomap, tahun):
     fig = go.Figure(
         data=go.Choropleth(
             geojson=indomap,
             locations=df["provinsi"],  # Spatial coordinates
             featureidkey="properties.state",
-            z=df["ipm"],  # Data to be color-coded
+            z=df[int(tahun)],  # Data to be color-coded
             colorscale="YlOrRd",
-            colorbar_title="ABCBRO",
+            colorbar_title="Rentang",
         )
     )
 
@@ -38,3 +55,82 @@ def plot_map(df, indomap):
     fig.data[0].colorbar.x=-0.05
 
     return fig
+
+def prediction(dfprov):
+    namakolom = dfprov.columns[0]
+
+    # TAHAP 1
+    X = dfprov.iloc[:, 1:12]
+    y = dfprov.iloc[:,0]
+    
+    X_train = X[:10]
+    y_train = y[:10]
+    X_test = X[10:]
+    y_test = y[10:]
+
+    params = {
+        "n_estimators": 1000,
+        "learning_rate": 0.01,
+        "loss": "squared_error",
+    }
+
+    returns = {}
+
+    reg = ensemble.GradientBoostingRegressor(**params)
+    reg.fit(X_train, y_train)
+    y_pred = reg.predict(X_test)
+
+    mse = mean_squared_error(y_test, y_pred)
+
+    returns['MSE'] = mse
+    returns['y_pred'] = y_pred
+    returns['y_test'] = y_test
+
+    feature_importance = reg.feature_importances_
+    importance_df = pd.DataFrame({'features': X_train.columns,
+                                'importance': feature_importance})
+    importance_df.sort_values(by='importance', ascending=False, inplace=True)
+    
+    returns['importance_df'] = importance_df
+
+    #select top 3 feature (misal)
+    feature=importance_df.iloc[:3,:1].T.values.tolist()
+    flat_list = [item for sublist in feature for item in sublist]
+    flat_list.append(namakolom)
+    dfprov = dfprov[dfprov.columns.intersection(flat_list)]
+
+    returns['dfprov'] = dfprov
+
+    # TAHAP 2
+    X = dfprov.iloc[:, 1:10]
+    y = dfprov[[namakolom]]
+
+    X_train = X[:10]
+    y_train = y[:10]
+    X_test = X[10:]
+    y_test = y[10:]
+
+    regressor = LinearRegression()
+    regressor.fit(X_train, y_train)
+
+    #persamaan
+    returns['regressor.intercept'] = regressor.intercept_
+    returns['regressor.coef_'] = regressor.coef_
+
+    y_pred = regressor.predict(X_test)
+    returns['y_pred Tahap 2'] = y_pred
+    returns['y_test Tahap 2'] = y_test
+
+    mae = mean_absolute_error(y_test, y_pred)
+    mse = mean_squared_error(y_test, y_pred)
+    rmse = np.sqrt(mse)
+
+    returns['mae Tahap 2'] = mae
+    returns['mse Tahap 2'] = mse
+    returns['rmse Tahap 2'] = rmse
+
+    return returns
+
+def is_feature_importance(temp):
+    if temp > 0.5:
+        return 'background-color: lightgreen'
