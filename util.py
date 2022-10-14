ac6811d793c85dce847f7dc4fdc4c7ba22a3c412
Haekal Rizky Yulianto <haekal.yulianto@outlook.com>
2022-10-15 01:21:27 +0700

    Update util.py

--- util.py
+++ util.py
@@ -2,9 +2,9 @@ import json
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
-from sklearn import datasets, ensemble
+from sklearn import ensemble
 from sklearn.metrics import mean_squared_error
-from sklearn.linear_model import LinearRegression, ElasticNet, SGDRegressor, BayesianRidge
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error
 
 def read_map(df_mapping, provinsi):
@@ -85,13 +85,13 @@ def prediction(dfprov):
     returns['y_test'] = y_test
 
     feature_importance = reg.feature_importances_
-    importance_df = pd.DataFrame({'Anggaran': X_train.columns,
+    importance_df = pd.DataFrame({'Fungsi Anggaran': X_train.columns,
                                 'Keutamaan': feature_importance})
     importance_df.sort_values(by='Keutamaan', ascending=False, inplace=True)
     
     returns['importance_df'] = importance_df
 
-    #select top 3 feature (misal)
+    #select top 3 feature
     feature=importance_df.iloc[:3,:1].T.values.tolist()
     flat_list = [item for sublist in feature for item in sublist]
     flat_list.append(namakolom)
@@ -107,11 +107,6 @@ def prediction(dfprov):
     y_train = y[:10]
     X_test = X[10:]
     y_test = y[10:]
-
-    # params2 = {
-    #     "learning_rate": 0.01,
-    #     "loss": "squared_error",
-    # }
 
     regressor = LinearRegression()
     regressor.fit(X_train, y_train)
@@ -132,8 +127,54 @@ def prediction(dfprov):
     returns['mse Tahap 2'] = mse
     returns['rmse Tahap 2'] = rmse
 
+    # TAHAP 3
+    # select top 1 feature
+    feature2=importance_df.iloc[:1,:1].T.values.tolist()
+    flat_list2 = [item for sublist in feature2 for item in sublist]
+    flat_list2.append(namakolom)
+    dfprov = dfprov[dfprov.columns.intersection(flat_list2)]
+
+    returns['dfprov2'] = dfprov
+
+    X = dfprov.iloc[:, 1:10]
+    y = dfprov[[namakolom]]
+
+    X_train = X[:10]
+    y_train = y[:10]
+    X_test = X[10:]
+    y_test = y[10:]
+
+    regressor = LinearRegression()
+    regressor.fit(X_train, y_train)
+
+    #persamaan
+    returns['regressor.intercept Tahap 3'] = regressor.intercept_
+    returns['regressor.coef_ Tahap 3'] = regressor.coef_
+
+    y_pred = regressor.predict(X_test)
+    returns['y_pred Tahap 3'] = y_pred
+    returns['y_test Tahap 3'] = y_test
+
+    mae = mean_absolute_error(y_test, y_pred)
+    mse = mean_squared_error(y_test, y_pred)
+    rmse = np.sqrt(mse)
+
+    returns['mae Tahap 3'] = mae
+    returns['mse Tahap 3'] = mse
+    returns['rmse Tahap 3'] = rmse
+
     return returns
 
 def is_feature_importance(temp):
     if temp > 0.5:
         return 'background-color: lightgreen'
+    elif temp > 0.2 and temp < 0.5:
+        return 'background-color: yellow'
+    elif temp > 0.1 and temp < 0.2:
+        return 'background-color: orange'
+    else:
+        return 'background-color: red'
+
+def is_target(value):
+    if value > 6:
+        return 'background-color: lightgreen' #Ini kudu diubah sih biar menyesuaikan target per indikatornya wkwk
